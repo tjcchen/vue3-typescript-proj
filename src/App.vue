7362934f602abef379be1a33e41c5cac02f68c78
Andy Chen <chenyangpoppin@hotmail.com>
2021-11-22 22:51:04 +0800

    feat: add ts support

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,22 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import { defineComponent } from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
 import {print} from './utils/utils';
 
-export default {
+export default defineComponent({
   name: 'App',
   components: {
     HelloWorld
   },
   mounted() {
-    print('Hello Vue3 and Typescript!');
+    print('hello vue3 + typescript!');
   }
-}
+});
 </script>
 
 <style>
